8143b47da57accb2f8caeb680c71f05800743037
immyjosh <immanueljoshuapaul@gmail.com>
2019-01-07 20:50:48 +0530

    added main page animation

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import Home from "./Components/Home";
+
 import Layout from "./Hoc/layout";
 
 function routes(props) {
   return (
     <Layout>
-      <Switch />
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
     </Layout>
   );
 }
